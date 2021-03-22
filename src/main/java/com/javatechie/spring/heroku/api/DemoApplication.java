327b311781a158eaa14d54acc97154fa4f0f91d9
Cylindo <skohliso@gmail.com>
2021-03-22 17:08:56 +0200

    Add files via upload
    latest changes

--- src/main/java/com/javatechie/spring/heroku/api/DemoApplication.java
+++ src/main/java/com/javatechie/spring/heroku/api/DemoApplication.java
@@ -3,6 +3,7 @@ package com.javatechie.spring.heroku.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
